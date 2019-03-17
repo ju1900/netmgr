4300fa673303f964d326e828d1b0f1c08e9c48a3
ju1900 <1044161599@qq.com>
2019-03-17 15:56:57 +0800

    *

--- testlist/static/testcase.js
+++
@@ -1,5 +0,0 @@
-$(function() {
-    // When we're using HTTPS, use WSS too.
-    var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
-    var chatsock = new WebSocket(ws_scheme + '://' + window.location.host + "/ws/testlist/testcase/");
-});
